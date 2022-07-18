9aa0afe4e2f6053e988ecfc1f50e6fe8ec15db86
monsterpolarbear <oliver.zangenberg@gmail.com>
2022-07-18 22:41:39 +0200

    update styles, scroll to anchor

--- fieldtrip.js
+++ fieldtrip.js
@@ -1,73 +1,87 @@
 "use strict";
 (function () {
-    /* 
-    * Load above closing </body> tag
-    */
     const ftHandleClicks = (e) => {
-        e.stopProagation();
-        e.preventDefault();
-        if (!e.target.id.startsWith('ft__')) {
-            return;
-        }
-        window.parent.postMessage({ id: e.target.id }, '*');
-        console.info("posting message ", e.target.id);
-    }
+		e.stopPropagation();
+		e.preventDefault();
+		if (!e.target.id.startsWith('ft__')) {
+			return;
+		}
+		// This seems to trigger the message eventListener, which should not listen
+		// to this browsing context
+		window.parent.postMessage({ id: e.target.id }, '*');
+		console.info('posting message ', e.target.id);
+	};
 
-    function ftSendAllClickables() {
-        const ftAll = document.querySelectorAll('[id^="ft__"]');
-        //obj = JSON.parse(JSON.stringify(ftAll));
-        const clickables = [];
-        for (let node of ftAll) {
-            clickables.push(node.id)
-        }
+	function ftSendAllClickables() {
+		const ftAll = document.querySelectorAll('[id^="ft__"]');
+		//obj = JSON.parse(JSON.stringify(ftAll));
+		const clickables = [];
+		for (let node of ftAll) {
+			clickables.push(node.id);
+		}
 
-        window.parent.postMessage({ all: clickables }, '*');
-    }
+		window.parent.postMessage({ all: clickables }, '*');
+	}
 
-    const ftHandleMessages = (e) => {
-        const ftAll = document.querySelectorAll('[id^="ft__"]');
-        const popoverNodes = document.querySelectorAll('[id^="foo__ft__"]');
-        switch (e.data.type) {
-            case 'highlight':
-                for (let ele of popoverNodes) {
-                    ele.remove()
-                }
-                // Add styles to highlight anchors
-                if (e.data.data.highlight) {
-                    // Add popper tooltip
-                    const popoverStyle = "position:relative;display:inline";
-                    const tooltipStyle = "position:absolute;padding:.5rem;bottom:0;left:0;z-index:100;background-color: rgba(255, 255, 255, .9);font-family: sans-serif;font-size: 1rem;font-weight: normal;font-style: normal;color: rgb(0, 0, 0);filter: drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06));";
-                    for (let ele of ftAll) {
+	const ftHandleMessages = (e) => {
+		const ftAll = document.querySelectorAll('[id^="ft__"]');
+		const popoverNodes = document.querySelectorAll('[id^="popover__ft__"]');
+		const tooltipNodes = document.querySelectorAll('[id^="tooltip__ft__"]');
+		switch (e.data.type) {
+			case 'highlight':
+				for (let ele of popoverNodes) {
+					ele.remove();
+				}
+				// Add styles to highlight anchors
+				if (e.data.highlight) {
+					// Add popper tooltip
+					for (let ele of ftAll) {
+						ele.classList.add('ft__highlight');
+						const popover = document.createElement('div');
+						const tooltip = document.createElement('div');
+						popover.id = `popover__${ele.id}`;
+						tooltip.id = `tooltip__${ele.id}`;
+						popover.classList.add('ft__popover');
+						tooltip.innerText = ele.id;
+						tooltip.classList.add('ft__popoverTooltip');
+						popover.appendChild(tooltip);
+						ele.appendChild(popover);
+					}
+				} else {
+					for (let ele of popoverNodes) {
+						ele.remove();
+					}
+					for (let ele of ftAll) {
+						ele.classList.remove('ft__highlight');
+					}
+				}
+				break;
+			case 'scrollTo':
+				const element = document.getElementById(e.data.anchorName);
+				const elTooltip = document.getElementById(`tooltip__${element.id}`);
+				if (!element) {
+					break;
+				}
+				element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+				for (let ele of ftAll) {
+					ele.classList.remove('ft__scrollTo');
+				}
+				for (let ele of tooltipNodes) {
+					ele.classList.remove('ft__tooltip__highlight');
+				}
 
-                        ele.classList.add('ft__highlight');
-                        const popover = document.createElement('div');
-                        const tooltip = document.createElement('div');
-                        popover.id = `foo__${ele.id}`;
-                        popover.style.cssText = popoverStyle;
-                        tooltip.style.cssText = tooltipStyle;
-                        tooltip.innerText = ele.id;
-                        popover.appendChild(tooltip);
-                        ele.appendChild(popover);
+				element.classList.add('ft__scrollTo');
+				elTooltip.classList.add('ft__tooltip__highlight');
+				break;
 
-                    }
+			default:
+				console.log('No Handler for message type');
+		}
 
-                } else {
-                    for (let ele of popoverNodes) {
-                        ele.remove()
-                    }
-                    for (let ele of ftAll) {
-                        ele.classList.remove('ft__highlight')
-                    }
-                }
-                break;
-            default:
-                console.log('No Handler for message type');
-        }
-
-        // add class
-        // e.data.removeHighlight
-        // Highlight All
-    }
+		// add class
+		// e.data.removeHighlight
+		// Highlight All
+	};
 
     document.body.addEventListener('click', ftHandleClicks, false);
     window.addEventListener('message', ftHandleMessages, false);
@@ -77,6 +91,6 @@
         document.body.removeEventListener('click', ftHandleClicks);
         window.removeEventListener('message', ftHandleMessages);
         return;
-    }
+    };
 
 }());
